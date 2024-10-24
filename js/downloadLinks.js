b6787e8215af3492b9f77723a86c746e278c2074
Tanush Golwala <tanushgolwala@gmail.com>
2024-10-24 20:32:12 +0530

    changed reponame

--- js/downloadLinks.js
+++ js/downloadLinks.js
@@ -1,23 +1,21 @@
-fetch("../latest.json").then(response => response.json()).then(data => { 
+fetch("../latest.json").then(response => response.json()).then(data => {
     console.log(data.version);
 
     const windowsAnchorTag = document.getElementById("windows-download-link");
     const linuxAnchorTag = document.getElementById("linux-download-link");
     const macAnchorTag = document.getElementById("mac-download-link");
     const androidAnchorTag = document.getElementById("android-download-link");
-    
+
     // The link to the download is a direct link to the file on the deployed repository
     // TODO: Change link to "`https://github.com/ACM-VIT/<cli-top-website>/..." once the repository is made under ACM-VIT organization
-    
-    windowsAnchorTag.href = `https://github.com/ManavMuthanna/cli-top-website/raw/main/buildFiles/v${data.version}/cli-top-windows_v${data.version}.zip`;
-    linuxAnchorTag.href = `https://github.com/ManavMuthanna/cli-top-website/raw/main/buildFiles/v${data.version}/cli-top-linux_v${data.version}.zip`;
-    macAnchorTag.href = `https://github.com/ManavMuthanna/cli-top-website/raw/main/buildFiles/v${data.version}/cli-top-macos_v${data.version}.zip`;
-    androidAnchorTag.href = `https://github.com/ManavMuthanna/cli-top-website/raw/main/buildFiles/v${data.version}/cli-top-android_v${data.version}.zip`;
+
+    windowsAnchorTag.href = `https://github.com/technical-director-acmvit/cli-top-website/raw/main/buildFiles/v${data.version}/cli-top-windows_v${data.version}.zip`;
+    linuxAnchorTag.href = `https://github.com/technical-director-acmvit/cli-top-website/raw/main/buildFiles/v${data.version}/cli-top-linux_v${data.version}.zip`;
+    macAnchorTag.href = `https://github.com/technical-director-acmvit/cli-top-website/raw/main/buildFiles/v${data.version}/cli-top-macos_v${data.version}.zip`;
+    androidAnchorTag.href = `https://github.com/technical-director-acmvit/cli-top-website/raw/main/buildFiles/v${data.version}/cli-top-android_v${data.version}.zip`;
 
     windowsAnchorTag.innerText = `v${data.version}`;
     linuxAnchorTag.innerText = `v${data.version}`;
     macAnchorTag.innerText = `v${data.version}`;
     androidAnchorTag.innerText = `v${data.version}`;
 });
-
-
