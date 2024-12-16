31838a6d1298cfe4fd0be05c4700db418f559f2e
theg <52027622+theg1239@users.noreply.github.com>
2024-12-16 10:17:33 +0530

    chore: typo

--- js/downloadLinks.js
+++ js/downloadLinks.js
@@ -14,7 +14,7 @@ fetch("../latest.json").then(response => response.json()).then(data => {
     linuxAnchorTag.href = `https://github.com/technical-director-acmvit/cli-top-website/raw/main/buildFiles/v${data.version}/cli-top-linux_v${data.version}.zip`;
     macAnchorTag.href = `https://github.com/technical-director-acmvit/cli-top-website/raw/main/buildFiles/v${data.version}/cli-top-macos_v${data.version}.zip`;
     androidAnchorTag.href = `https://github.com/technical-director-acmvit/cli-top-website/raw/main/buildFiles/v${data.version}/cli-top-android_v${data.version}.zip`;
-    windowsInstallerAnchorTag.href = `https://github.cim/technical-director-acmvit/cli-top-website/raw/main/buildFiles/v${data.version}/cli-top-windows-installer_v${data.version}.exe`;
+    windowsInstallerAnchorTag.href = `https://github.com/technical-director-acmvit/cli-top-website/raw/main/buildFiles/v${data.version}/cli-top-windows-installer_v${data.version}.exe`;
 
     windowsAnchorTag.innerText = `v${data.version}`;
     linuxAnchorTag.innerText = `v${data.version}`;
